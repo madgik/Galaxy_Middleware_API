ec14b9f46fd44cc802e27b5180fa5e013e4e5c61
Konstantinos <kostas24061992@yahoo.com>
2019-06-12 12:08:28 +0300

    Fix IP. KK
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   .idea/artifacts/Gradle___gr_di_uoa_kk_gatewayApi___Gateway_API_1_0_0_SNAPSHOT_war__exploded_.xml
    	modified:   .idea/modules/Gateway_API_main.iml
    	modified:   .idea/modules/Gateway_API_test.iml
    	modified:   build.gradle
    	modified:   src/main/java/gr/di/uoa/kk/gatewayApi/RestControllers/Retrofit/RetrofitClientInstance.java

--- src/main/java/gr/di/uoa/kk/gatewayApi/RestControllers/Retrofit/RetrofitClientInstance.java
+++ src/main/java/gr/di/uoa/kk/gatewayApi/RestControllers/Retrofit/RetrofitClientInstance.java
@@ -7,14 +7,19 @@ package gr.di.uoa.kk.gatewayApi.RestControllers.Retrofit;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientInstance {
 
-    private static Retrofit retrofit;
-    private static final String BASE_URL = "http://88.197.53.102/api/";
+    //The galaxy URL
+    @Value("${kk.app.galaxyURL}")
+    private static String url;
 
+    private static Retrofit retrofit;
+
+    private static final String BASE_URL = url + "/api/";
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
 
